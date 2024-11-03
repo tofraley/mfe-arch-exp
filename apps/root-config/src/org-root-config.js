262816cf2e5586db12a2a05ba6a0c308b9dec94f
taylorfraley <taylor.fraley@xplortechnologies.com>
2024-11-02 18:20:38 -0700

    cleanup & add comments

--- apps/root-config/src/org-root-config.js
+++ apps/root-config/src/org-root-config.js
@@ -1,29 +1,25 @@
 /* eslint-disable no-console */
-import {
-  navigateToUrl,
-  pathToActiveWhen,
-  registerApplication,
-  start,
-} from "single-spa";
+import { navigateToUrl, registerApplication, start } from "single-spa";
 import {
   constructApplications,
   constructRoutes,
   constructLayoutEngine,
 } from "single-spa-layout";
 import microfrontendLayout from "./microfrontend-layout.html";
-import { AuthManager } from "lib";
+import AuthManager from "lib";
 
 const res = await fetch("/auth.json");
 
 if (!res.ok) throw Error("Could not load auth config");
 
-const authConf = await res.json();
-const authManager = new AuthManager(authConf);
+const authManager = new AuthManager(await res.json());
 
 // Handle multiple cleanup scenarios
 window.addEventListener("unload", () => authManager.destroy());
 window.addEventListener("beforeunload", () => authManager.destroy());
 
+// Attaching an object with auth methods to window
+// as a simple way to share across microfrontends
 window.AuthService = {
   checkAuth: authManager.checkAuth,
   login: authManager.login,
@@ -31,29 +27,26 @@ window.AuthService = {
   timeLeft: authManager.timeLeft,
 };
 
-// the rest of the root-config
 const routes = constructRoutes(microfrontendLayout);
 const applications = constructApplications({
   routes,
   loadApp({ name }) {
-    // console.log("app name", name);
-    // if (name === "@org/login") return System.import(name);
-    // if (!authManager.checkAuth()) {
-    //   navigateToUrl("/login");
-    //   return false;
-    // }
     return System.import(name);
   },
 });
 
+// Listening for specific single-spa routing events
 window.addEventListener("single-spa:before-routing-event", async () => {
   const path = window.location.pathname;
-  console.log("navigating to", path);
+
+  // Handle any routes needed during login flow, i.e. post sign-in redirect
   if (path == "/auth/post-login") {
     const user = await authManager.handleSigninCallback();
     console.log("user", user);
     navigateToUrl("/");
   }
+
+  // This works like and auth guard
   if (path !== "/login" && !authManager.checkAuth()) {
     navigateToUrl("/login");
   }
