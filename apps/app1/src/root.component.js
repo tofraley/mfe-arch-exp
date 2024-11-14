238a32bf499ef058d03cac6342a2d485ae24205d
Taylor Fraley <taylor.fraley@xplortechnologies.com>
2024-11-14 18:21:39 -0500

    move some code to auth-manager.js

--- apps/app1/src/root.component.js
+++ apps/app1/src/root.component.js
@@ -13,6 +13,9 @@ export default function Root(props) {
       <h1>Welcome to the app!</h1>
       <p>Time left: {timeLeft}</p>
       <button onClick={() => window.AuthService.logout()}>Logout</button>
+      <button onClick={() => window.AuthService.checkAuth()}>Check Auth</button>
+      <button onClick={() => window.AuthService.navTo("/login")}>Login</button>
+      <a href="login">Go!</a>
     </div>
   );
 }
--- apps/root-config/src/org-root-config.js
+++ apps/root-config/src/org-root-config.js
@@ -1,59 +1,66 @@
 /* eslint-disable no-console */
-import { navigateToUrl, registerApplication, start } from "single-spa";
+import { registerApplication, start } from "single-spa";
 import {
   constructApplications,
   constructRoutes,
   constructLayoutEngine,
 } from "single-spa-layout";
 import microfrontendLayout from "./microfrontend-layout.html";
-import AuthManager from "lib";
+import { getAuthManager } from "lib";
 
-const res = await fetch("/auth.json");
+const authManager = await getAuthManager();
 
-if (!res.ok) throw Error("Could not load auth config");
+function doStart() {
+  // Constructing the routes and applications from the layout
+  const routes = constructRoutes(microfrontendLayout);
+  const applications = constructApplications({
+    routes,
+    loadApp({ name }) {
+      return System.import(name);
+    },
+  });
 
-const authManager = new AuthManager(await res.json());
+  const layoutEngine = constructLayoutEngine({ routes, applications });
 
-// Handle multiple cleanup scenarios
-window.addEventListener("unload", () => authManager.destroy());
-window.addEventListener("beforeunload", () => authManager.destroy());
+  applications.forEach(registerApplication);
+  layoutEngine.activate();
+  start();
+}
 
-// Attaching an object with auth methods to window
-// as a simple way to share across microfrontends
-window.AuthService = {
-  checkAuth: authManager.checkAuth,
-  login: authManager.login,
-  logout: authManager.logout,
-  timeLeft: authManager.timeLeft,
-};
+const path = window.location.pathname;
+// // Listening for specific single-spa routing events
+// window.addEventListener("single-spa:before-routing-event", async () => {
+//   const path = window.location.pathname;
 
-const routes = constructRoutes(microfrontendLayout);
-const applications = constructApplications({
-  routes,
-  loadApp({ name }) {
-    return System.import(name);
-  },
-});
+//   // Handle any routes needed during login flow, i.e. post sign-in redirect
+//   if (path == "/auth/post-login") {
+//     const user = await authManagerInstance.handleSigninCallback(doStart);
+//     // doStart();
+//   }
 
-// Listening for specific single-spa routing events
-window.addEventListener("single-spa:before-routing-event", async () => {
-  const path = window.location.pathname;
+//   // // This works like and auth guard
+//   // if (path !== "/login" && !authManagerInstance.checkAuth()) {
+//   //   navigateToUrl("/login");
+//   //   // authManagerInstance.login();
+//   // }
+// });
 
-  // Handle any routes needed during login flow, i.e. post sign-in redirect
-  if (path == "/auth/post-login") {
-    const user = await authManager.handleSigninCallback();
-    console.log("user", user);
-    navigateToUrl("/");
-  }
+// Handle any routes needed during login flow, i.e. post sign-in redirect
+switch (path) {
+  case "/auth/post-login":
+    const user = await authManager.handleSigninCallback(doStart);
+    break;
+  default:
+    if (!(await authManager.checkAuth())) {
+      authManager.login(path);
+    } else {
+      doStart();
+    }
+    break;
+}
 
-  // This works like and auth guard
-  if (path !== "/login" && !authManager.checkAuth()) {
-    navigateToUrl("/login");
-  }
-});
-
-const layoutEngine = constructLayoutEngine({ routes, applications });
-
-applications.forEach(registerApplication);
-layoutEngine.activate();
-start();
+// if (path == "/auth/post-login") {
+//   const user = await authManager.handleSigninCallback(doStart);
+// } else if (!authManager.checkAuth()) {
+//   authManager.login();
+// }
